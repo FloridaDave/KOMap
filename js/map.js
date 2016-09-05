40d632f354d253586cd76ba3d2fa41660bf60494
FloridaDave <tohighdesertdave@yahoo.com>
2016-09-05 14:41:43 -0400

    deleted old array reference

--- js/map.js
+++ js/map.js
@@ -7,20 +7,6 @@ var markers = [];
 
 // Hard coded locations array for model section
 
-	// var locations = [
-	// 	{title: 'Where I live now', location: {lat: 28.526289, lng: -81.542796}},
-	// 	{title: 'Ellie Lou BBQ', location: {lat: 28.531206, lng: -81.539861}},
-	// 	{title: 'Pho-Real', location: {lat: 28.529005, lng: -81.541229}},
-	// 	{title: 'Keke Breakfast Cafe', location: {lat: 28.529887, lng: -81.540118}},
-	// 	{title: 'Wells Fargo Bank', location: {lat: 28.532100, lng: -81.541391}},
-	// 	{title: 'Trustco Bank', location: {lat: 28.528313, lng: -81.542554}},
-	// 	{title: 'West Orange 5 Theater', location: {lat: 28.548660, lng: -81.542499}},
-	// 	{title: 'AMC West Theater', location: {lat: 28.551897, lng: -81.515219}},
-	// 	{title: 'George Bailey Park', location: {lat: 28.528546, lng: -81.556786}},
-	// 	{title: 'West Orange Dog Park', location: {lat: 28.543861, lng: -81.564208}},
-	// 	{title: 'Butler Bay Recreation Area', location: {lat: 28.505466, lng: -81.551895}},
-	// ]
-
 	// KO Array
 
 var locations = [
