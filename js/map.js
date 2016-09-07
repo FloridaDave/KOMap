89e68921e157e89151ad00c15c59725f83876068
FloridaDave <tohighdesertdave@yahoo.com>
2016-09-07 11:13:47 -0400

    removed 10 locations to simplify code to build upon

--- js/map.js
+++ js/map.js
@@ -9,75 +9,21 @@ var markers = [];
 
 	// KO Array
 
+	// Removed 10 locations to simplify code - NOTE: removed comma from first {}
+
 var locations = [
 	{ 'title': 'Where I Live Now',
 	  'lat': 28.526289,
 	  'lng': -81.542796,
-	  'Info': [{'what': 'Home','type':'Apartment'}]
-	},
-
-	{ 'title': 'Ellie Lou BBQ',
-	  'lat': 28.531206,
-	  'lng': -81.539861,
-	  'Info': [{'what': 'Restaurant','type':'BBQ'}]
-	},
-
-	{ 'title': 'Pho-Real',
-	  'lat': 28.529005,
-	  'lng': -81.541229,
-	  'Info': [{'what': 'Restaurant','type':'Vietnamese'}]
-	},
-
-	{ 'title': 'Keke Breakfast Cafe',
-	  'lat': 28.529887,
-	  'lng': -81.540118,
-	  'Info': [{'what': 'Restaurant','type':'Breakfast'}]
-	},
-
-	{ 'title': 'Wells Fargo Bank',
-	  'lat': 28.532100,
-	  'lng': -81.541391,
-	  'Info': [{'what': 'Bank','type':'Branch Office'}]
-	},
-
-	{ 'title': 'Trustco Bank',
-	  'lat': 28.528313,
-	  'lng': -81.542554,
-	  'Info': [{'what': 'Bank','type':'Branch Office'}]
-	},
-
-	{ 'title': 'West Orange 5 Theater',
-	  'lat': 28.548660,
-	  'lng': -81.542499,
-	  'Info': [{'what': 'Entertainment','type':'Movie Theater'}]
-	},
-
-	{ 'title': 'AMC West Theater',
-	  'lat': 28.551897,
-	  'lng': -81.515219,
-	  'Info': [{'what': 'Entertainment','type':'Movie Theater'}]
-	},
-
-	{ 'title': 'George Bailey Park',
-	  'lat': 28.528546,
-	  'lng': -81.556786,
-	  'Info': [{'what': 'Recreation','type':'Park'}]
-	},
-
-	{ 'title': 'West Orange Dog Park',
-	  'lat': 28.543861,
-	  'lng': -81.564208,
-	  'Info': [{'what': 'Recreation','type':'Park'}]
-	},
-
-	{ 'title': 'Butler Bay Recreation Area',
-	  'lat': 28.505466,
-	  'lng': -81.551895,
-	  'Info': [{'what': 'Recreation','type':'Park'}]
+	  'info': [{'what': 'Home','type':'Apartment'}]
 	}
 ];
 
 //*** Octopus
+
+
+
+
 
 var ViewModel = function (){
 	    var self = this;
@@ -128,6 +74,17 @@ var ViewModel = function (){
 			infoWindow.setContent('<div>' + marker.title + '</div>');
 			infoWindow.open(map, marker);
 		});
+
+		//*** View
+
+		function Places (title, lat, lng)
+		{
+				this.title = title;
+				this.lat = lat;
+				this.lng = lng;
+				this.info = info
+
+		}
 
 
 		// This function populates the infowindow when the marker is clicked. Only one window 
