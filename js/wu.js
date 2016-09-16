d00bb951705bdda4bcbfe2d94d6c3b0f007ee66a
FloridaDave <tohighdesertdave@yahoo.com>
2016-09-16 11:17:02 -0400

    included general weather and humidity code for wu.js

--- js/wu.js
+++ js/wu.js
@@ -5,16 +5,16 @@ weather.send(null);
 var r = JSON.parse(weather.response);
 
 
-// var weather = r.current_observation.weather + "";
+var weather = r.current_observation.weather + "";
 var temp = r.current_observation.temp_f + "";
 var wind = r.current_observation.wind_string + "";
-// var humidity = r.current_observation.relative_humidity + "";
+var humidity = r.current_observation.relative_humidity + "";
 
 function getWeather(id,res) {
   document.getElementById(id).innerHTML=res;
 }
 
-// getWeather("weather",weather);
+getWeather("weather",weather);
 getWeather("temp",temp);
 getWeather("wind",wind);
-// getWeather("humidity",humidity);
+getWeather("humidity",humidity);
