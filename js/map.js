5cb3ccfa27b8461f8e34b19a254fb6b6dd8b6c9e
FloridaDave <tohighdesertdave@yahoo.com>
2016-09-16 21:46:21 -0400

    first try to add more infoWindow info - still says undefined

--- js/map.js
+++ js/map.js
@@ -9,68 +9,67 @@ var myViewModel;
 var locations = [ 	
 	{ 'title': 'Where I Live Now',
 	  'latlng': {'lat': 28.526289, 'lng': -81.542796},
-	  'Info': 'Casa Mirella Apartments',
+	  'info': 'Casa Mirella Apartments',
 	
 	},
 
 	{ 'title': 'Ellie Lou BBQ',
 	  'latlng': {'lat': 28.531206, 'lng': -81.539861},
-	  'Info': 'Great BBQ Restaurant',
+	  'info': 'Great BBQ Restaurant',
 	  
 	},
 
 	{ 'title': 'Pho-Real',
 	  'latlng': {'lat': 28.529005, 'lng': -81.541229},
-	  'Info': 'Great Vietnamese Restaurant',
+	  'info': 'Great Vietnamese Restaurant',
 
 	},
 
 	{ 'title': 'Keke Breakfast Cafe',
 	  'latlng': {'lat': 28.529887, 'lng': -81.540118},
-	  'Info': 'Cool Restaurant for Breakfast',
+	  'info': 'Cool Restaurant for Breakfast',
 	  
 	},
 
 	{ 'title': 'Wells Fargo Bank',
 	  'latlng': {'lat': 28.532100, 'lng': -81.541391},
-	  'Info': 'Bank with recent HUGE scandal',
+	  'info': 'Bank with recent HUGE scandal',
 	  
 	},
 
 	{ 'title': 'Trustco Bank',
 	  'latlng': {'lat': 28.528313, 'lng': -81.542554},
-	  'Info': 'Very small bank - looks kind of strange',
+	  'info': 'Very small bank - looks kind of strange',
 	
-	  // 'Info': [{'what': 'Bank','type':'Branch Office'}]
 	},
 
 	{ 'title': 'West Orange 5 Theater',
 	  'latlng': {'lat': 28.548660, 'lng': -81.542499},
-	  'Info': 'Funky theather with Great Prices',
+	  'info': 'Funky theather with Great Prices',
 	
 	},
 
 	{ 'title': 'AMC West Theater',
 	  'latlng': {'lat': 28.551897, 'lng': -81.515219},
-	  'Info': 'Theater Chain'
+	  'info': 'Theater Chain'
 	 
 	},
 
 	{ 'title': 'George Bailey Park',
 	  'latlng': {'lat': 28.528546, 'lng': -81.556786},
-	  'Info': 'Park with Baseball and Soccer Fields',
+	  'info': 'Park with Baseball and Soccer Fields',
 
 	},
 
 	{ 'title': 'West Orange Dog Park',
 	  'latlng': {'lat': 28.543861, 'lng': -81.564208},
-	  'Info': 'Coolest Dog Park in Florida',
+	  'info': 'Coolest Dog Park in Florida',
 
 	},
 
 	{ 'title': 'Butler Bay Recreation Area',
 	  'latlng': {'lat': 28.505466, 'lng': -81.551895},
-	  'Info': 'Great Water Front Park with Tennis',
+	  'info': 'Great Water Front Park with Tennis',
 	  
 	}
 ];
@@ -116,18 +115,7 @@ var ViewModel = function (){
 			});
 		}
 	});
-
- //    	var filter = this.filter().toLowerCase();
-	//     if (!filter) {
-	//         return this.items();
-	//     } else {
-	//         return ko.utils.arrayFilter(this.items(), function(item) {
-	//             return ko.utils.stringStartsWith(item.name().toLowerCase(), filter);
-	//         });
-	//     }
-	// }, viewModel);
-
-
+	
 
 	self.triggerMarker = function(location){
 		var marker = location.marker;  
@@ -171,7 +159,7 @@ function initMap(){
 					// populateInfoWindow(this, infoWindow);
 					var marker = this;
 					infoWindow.setContent(
-						'<div>' + marker.title + '</div>');
+						'<div>' + marker.title + '<br>' + marker.info + '</div>');
 					infoWindow.open(map, marker);
 					this.setAnimation(google.maps.Animation.BOUNCE);
 						setTimeout(function(){ marker.setAnimation(null); }, 1420);
